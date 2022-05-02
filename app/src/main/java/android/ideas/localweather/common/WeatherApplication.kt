770ef77ee4640cac0fac4cf5f9b1f8bdfac00269
JongDo-android <whdgus090@gmail.com>
2022-05-02 16:06:02 +0900

    feat: HiltAndroid annotation 설정

---
+++ app/src/main/java/android/ideas/localweather/common/WeatherApplication.kt
@@ -0,0 +1,8 @@
+package android.ideas.localweather.common
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication: Application() {
+}
