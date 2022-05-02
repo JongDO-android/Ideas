3169acf97d68c622734e4b6d4781209eae73433a
JongDo-android <whdgus090@gmail.com>
2022-05-02 16:03:47 +0900

    feat: WeatherRepository interface 구현체 추가

---
+++ app/src/main/java/android/ideas/localweather/repository/WeatherRepository.kt
@@ -0,0 +1,27 @@
+package android.ideas.localweather.repository
+
+import android.ideas.localweather.remote.Result
+import android.ideas.localweather.remote.RetrofitBuilder
+import android.ideas.localweather.remote.dto.ConsolidatedWeather
+import android.ideas.localweather.remote.dto.LocalInformation
+import retrofit2.HttpException
+
+class WeatherRepository: RemoteRepository {
+    private val retrofit = RetrofitBuilder.createService()
+    override suspend fun fetchLocalInfo(query: String): Result<List<LocalInformation>> {
+        return try {
+            Result.Success(retrofit.fetchLocalInfo(query))
+        } catch (e: HttpException) {
+            Result.Error(e)
+        }
+    }
+
+    override suspend fun fetchWeatherInfo(woeId: Long): Result<ConsolidatedWeather> {
+        return try {
+            Result.Success(retrofit.fetchLocalWeather(woeId))
+        } catch (e: HttpException) {
+            Result.Error(e)
+        }
+    }
+
+}
