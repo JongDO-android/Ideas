9f2ac0f214235371c15d6a17e07849760f4bb910
JongDo-android <whdgus090@gmail.com>
2022-05-02 12:57:31 +0900

    feat: LocalWeatherService 추가

---
+++ app/src/main/java/android/ideas/localweather/remote/LocalWeatherService.kt
@@ -0,0 +1,14 @@
+package android.ideas.localweather.remote
+
+import android.ideas.localweather.remote.dto.LocalInformation
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface LocalWeatherService {
+    @GET("/api/location/search/")
+    fun fetchLocalInfo(@Query("query") query: String): LocalInformation
+
+    @GET("/api/location/{woeid}")
+    fun fetchLocalWeather(@Path("woeid") woeId: Long)
+}
