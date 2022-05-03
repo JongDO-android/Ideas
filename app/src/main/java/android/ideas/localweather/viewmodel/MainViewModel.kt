e652111715473623fb7de8cb62b5c51729344c0b
JongDo-android <whdgus090@gmail.com>
2022-05-03 17:44:21 +0900

    fix: MainViewModel API Request 로직 수정

--- app/src/main/java/android/ideas/localweather/viewmodel/MainViewModel.kt
+++ app/src/main/java/android/ideas/localweather/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package android.ideas.localweather.viewmodel
 
+import android.ideas.localweather.data.WeatherItem
 import android.ideas.localweather.remote.Result
 import android.ideas.localweather.remote.dto.LocalWeather
 import android.ideas.localweather.repository.RemoteRepository
@@ -18,48 +19,61 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val repository: RemoteRepository
 ): ViewModel() {
-    private val tempLocalWeathers = mutableListOf<LocalWeather>()
-    private val _localWeathers = MutableLiveData<List<LocalWeather>>()
-    val localWeather: LiveData<List<LocalWeather>> get() = _localWeathers
+    private val weatherItems = mutableListOf<WeatherItem>()
+    private val _localWeathers = MutableLiveData<List<WeatherItem>>()
+    val localWeather: LiveData<List<WeatherItem>> get() = _localWeathers
 
     private val _isRequestDone = MutableLiveData(false)
     val isRequestDone: LiveData<Boolean> get() = _isRequestDone
 
     fun fetchLocalInfo() = viewModelScope.launch {
+        weatherItemsInit()
         _isRequestDone.value = false
         val result = withContext(Dispatchers.IO) {
             repository.fetchLocalInfo(DEFAULT_QUERY)
         }
         when(result) {
             is Result.Success -> {
-                fetchWeatherInfo(result.data.map { it.woeId })
+                result.data.map {
+                    fetchWeatherInfo(it.title, it.woeId)
+                }
+            }
+            is Result.Error -> {
+            }
+        }
+
+        _isRequestDone.value = true
+    }
+
+    private fun fetchWeatherInfo(title: String, woeId: Long) = viewModelScope.launch {
+        val result = withContext(Dispatchers.IO) {
+            repository.fetchWeatherInfo(woeId)
+        }
+        when(result) {
+            is Result.Success -> {
+                weatherItems.add(WeatherItem.WeatherLocal(title))
+                weatherItems.add(WeatherItem.Weather(result.data[0]))
+                weatherItems.add(WeatherItem.Weather(result.data[1]))
             }
             is Result.Error -> {
 
             }
         }
-        _isRequestDone.value = true
+        _localWeathers.value = weatherItems.toList()
     }
 
-    private fun fetchWeatherInfo(woeIds: List<Long>) = viewModelScope.launch {
-        withContext(Dispatchers.IO) {
-            woeIds.forEach { woeId ->
-                when(val result = repository.fetchWeatherInfo(woeId)) {
-                    is Result.Success -> {
-                        tempLocalWeathers.add(result.data[0])
-                        tempLocalWeathers.add(result.data[1])
-                    }
-                    is Result.Error -> {
-
-                    }
-                }
-            }
-        }
-        Log.i("MainViewModel :", "${tempLocalWeathers.size}")
-        _localWeathers.value = tempLocalWeathers.toList()
+    private fun weatherItemsInit() {
+        weatherItems.clear()
+        _localWeathers.value = weatherItems.toList()
+        weatherItems.add(WeatherItem.Title(LOCAL))
+        weatherItems.add(WeatherItem.Title(TODAY))
+        weatherItems.add(WeatherItem.Title(TOMORROW))
     }
 
     companion object {
         const val DEFAULT_QUERY = "se"
+        const val LOCAL = "Local"
+        const val TODAY = "Today"
+        const val TOMORROW = "Tomorrow"
     }
 }
