6a080cae5750aac7763ba908d237c2d3f1ec42f2
JongDo-android <whdgus090@gmail.com>
2022-05-03 17:44:48 +0900

    feat: MainActivity Adapter 설정 추가

--- app/src/main/java/android/ideas/localweather/ui/MainActivity.kt
+++ app/src/main/java/android/ideas/localweather/ui/MainActivity.kt
@@ -2,15 +2,46 @@ package android.ideas.localweather.ui
 
 import android.ideas.localweather.R
 import android.ideas.localweather.databinding.ActivityMainBinding
+import android.ideas.localweather.ui.adapter.WeatherAdapter
+import android.ideas.localweather.viewmodel.MainViewModel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val weatherAdapter: WeatherAdapter by lazy {
+        WeatherAdapter()
+    }
+    private val mainViewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.lifecycleOwner = this
+        setBindingAttr()
+        setUiAttr()
+    }
+
+    private fun setBindingAttr() {
+        with(binding) {
+            lifecycleOwner = this@MainActivity
+            viewModel = mainViewModel
+        }
+    }
+
+    private fun setUiAttr() {
+        mainViewModel.fetchLocalInfo()
+
+        with(binding) {
+            val gridLayoutManager = GridLayoutManager(this@MainActivity, 3)
+            rvLocalWeather.layoutManager = gridLayoutManager
+            rvLocalWeather.adapter = weatherAdapter
+        }
+        mainViewModel.localWeather.observe(this) {
+            weatherAdapter.submitList(it)
+        }
     }
 }
